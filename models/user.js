4cdf09ed6572359187eb2c7c0e09b13893f32b3a
Tyson Anderson <tysonda621@gmail.com>
2022-08-12 15:39:29 -0700

    get the Oauth working properly

--- models/user.js
+++ models/user.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose');
 
 // Create your User Model
+const userSchema = new mongoose.Schema({
+	googleId: {
+	  type: String,
+	  required: true
+	},
+	email: String,
+  }, {
+	timestamps: true
+  });
+
+  module.exports = mongoose.model('User1', userSchema)
--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,15 @@ router.get('/', function(req, res) {
   // in the student demo this was res.redirect('/movies'), what do you want?
   // This could be a landing page, or just redirect to your main resource page which you'll have an a tag that makes 
   // a request to `/auth/google` route below
+  res.render('../views/index')
 });
 
 // Google OAuth login route
 router.get('/auth/google', passport.authenticate(
   'google',
-  { scope: ['profile', 'email'] }
+  { 
+    scope: ['profile', 'email'] 
+  }
 ));
 
 // Google OAuth callback route
@@ -28,7 +31,7 @@ router.get('/oauth2callback', passport.authenticate(
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout(function(){ //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
-    res.redirect('/'). // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
+    res.redirect('/') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
   })
 })
 
