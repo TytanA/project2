7f0d6c128959871eef46d4243749046f25a22482
Tyson Anderson <tysonda621@gmail.com>
2022-08-16 16:22:22 -0700

    add update session button, route, controller. run tests, seems to be working expect the redirect.

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,12 +1,52 @@
-const Campaign = require('../models/campaign')
+const Campaign = require('../models/campaign');
 
 module.exports = {
     create,
-    delete: deleteSession
+    delete: deleteSession,
+    edit,
+    update
+}
+
+async function update(req, res){
+    console.log(req.params.id, '<-req.params.id')
+    try{
+        const campaignDocument = await Campaign.findOne({
+            'gameSession._id': req.params.id,
+            
+        })
+        const sessionDocument = campaignDocument.gameSession.id(req.params.id);
+
+        sessionDocument.sessionTitle = req.body.sessionTitle;
+        sessionDocument.encounters = req.body.encounters;
+        sessionDocument.treasure = req.body.treasure;
+        sessionDocument.sessionDescrip = req.body.sessionDescrip;
+        campaignDocument.save()
+        res.redirect(`campaigns/${campaignDocument._id}`)
+    }catch(err){
+        res.send(err)
+    }
+}
+
+async function edit(req, res){
+    try{
+        const campaignDocument = await Campaign.findOne({
+            'gameSession._id': req.params.id,
+            
+        })
+        const sessionDocument = campaignDocument.gameSession.id(req.params.id);
+
+        res.render(`sessions/edit`, {
+            campaign: campaignDocument,
+            gSession: sessionDocument
+        })
+    
+    }catch(err){
+        res.send(err)
+    }
 }
 
 async function create(req, res){
-console.log(req.body, '<-here is the sessions stuff')
+
     try{
         let campaignDocument = await Campaign.findById(req.params.id);
         campaignDocument.gameSession.push(req.body);
@@ -26,7 +66,7 @@ async function deleteSession(req, res){
         });
         campaignDocument.gameSession.remove(req.params.id)
         await campaignDocument.save()
-        res.redirect(`/campaign`)
+        res.redirect(`/campaigns/${campaignDocument._id}`)
     }catch(err){
         res.send(err)
     }
--- routes/sessions.js
+++ routes/sessions.js
@@ -4,7 +4,10 @@ const passport = require('passport');
 const sessionController = require('../controllers/sessions');
 const isLoggedIn = require('../config/auth');
 
+
+router.get('/sessions/:id/edit', sessionController.edit);
 router.post('/campaigns/:id/sessions', sessionController.create);
+router.put('/sessions/:id', sessionController.update);
 router.delete('/sessions/:id', sessionController.delete);
 
 module.exports = router;
